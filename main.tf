5010f6b629d63d2319d795c7e2b9e097e168999a
Aleksander Fidelus <63016446+FidelusAleksander@users.noreply.github.com>
2025-08-02 19:36:51 +0000

    feat: update devcontainer and main.tf for GitHub repository integration

--- main.tf
+++ main.tf
@@ -9,18 +9,19 @@ terraform {
 
 provider "github" {}
 
-
+data "github_repository" "this" {
+  full_name = var.repository_full_name
+}
 
 resource "github_issue_label" "test_repo" {
-  repository = var.repository_name
+  repository = data.github_repository.this.name
   name       = "urgent"
   color      = "FF0000"
   description = "This is an urgent issue that needs immediate attention."
 }
 
 
-variable "repository_name" {
-  description = "The name of the GitHub repository"
+variable "repository_full_name" {
+  description = "The name of this GitHub repository"
   type        = string
-  default     = "introduction-to-terraform"
 }
