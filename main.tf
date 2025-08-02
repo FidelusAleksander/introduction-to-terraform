b5552f1249ffb06f2b198b158c8687f0afe3ab02
Aleksander Fidelus <63016446+FidelusAleksander@users.noreply.github.com>
2025-08-02 15:09:39 +0000

    Start with urgent label

--- main.tf
+++ main.tf
@@ -7,39 +7,20 @@ terraform {
   }
 }
 
-provider "github" {
+provider "github" {}
+
+
+
+resource "github_issue_label" "test_repo" {
+  repository = var.repository_name
+  name       = "urgent"
+  color      = "FF0000"
+  description = "This is an urgent issue that needs immediate attention."
 }
 
-data "github_repository" "current" {
-  full_name = "FidelusAleksander/introduction-to-terraform"
+
+variable "repository_name" {
+  description = "The name of the GitHub repository"
+  type        = string
+  default     = "introduction-to-terraform"
 }
-
-output "repository" {
-  value = data.github_repository.current
-  
-}
-
-
-# resource "github_repository_ruleset" "protect_default_branch" {
-#   name        = "Protect Default Branch"
-#   repository  = data.github_repository.current.name
-#   target      = "branch"
-#   enforcement = "active"
-
-#   conditions {
-#     ref_name {
-#       include = ["~DEFAULT_BRANCH"]
-#       exclude = []
-#     }
-#   }
-
-#   rules {
-#     pull_request {
-#       required_approving_review_count = 1
-#       dismiss_stale_reviews_on_push   = true
-#       require_code_owner_review       = false
-#       require_last_push_approval      = false
-#     }
-#   }
-# }
-
